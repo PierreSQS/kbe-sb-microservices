9fbbd0eb19d7935a1354212632802e25efde0363
PierreSQS <nomo_messina@web.de>
2024-08-13 01:21:19 +0200

    Sec57_Chap503-XXX: security code optimization (SB3.3.2)

--- sb-services/kbe-brewery-inventory-service/src/main/java/guru/sfg/brewery/inventory_service/config/WebSecurityConfig.java
+++ sb-services/kbe-brewery-inventory-service/src/main/java/guru/sfg/brewery/inventory_service/config/WebSecurityConfig.java
@@ -1,21 +1,32 @@
 package guru.sfg.brewery.inventory_service.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * Created by jt on 5/16/20.
+ * Modified by Pierrot on 2024-08-12.
  */
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().mvcMatchers("/actuator/health/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        HttpSecurity httpSecurity = http.authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers("/actuator/health/**").permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
+
+
+//        HttpSecurity httpSecurity1 = http.securityMatcher(EndpointRequest.toAnyEndpoint())
+//                .authorizeHttpRequests(authorize ->
+//                        authorize.anyRequest().permitAll())
+//                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
     }
 }
